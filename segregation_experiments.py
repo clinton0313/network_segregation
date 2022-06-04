ffb4c2021ae8fb2d8f782da0ef71d379efff6183
clinton0313 <clinton0313@gmail.com>
2022-06-04 21:01:34 +0200

    Gitignore of pkl files

--- segregation_experiments.py
+++ segregation_experiments.py
@@ -1,9 +1,13 @@
 #%%
 import itertools
+<<<<<<< HEAD
 import random
+=======
+>>>>>>> c3b7eeb... Ran first few experiments
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pickle
 import pandas as pd
 import pickle
 
@@ -16,9 +20,12 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 def run_experiment(rounds: int, file: str, seed: int = 0, **env_args):
 
+<<<<<<< HEAD
     np.random.seed(seed)
     random.seed(seed)
 
+=======
+>>>>>>> c3b7eeb... Ran first few experiments
     if not os.path.isfile(file):
         data = pd.DataFrame(
             columns = (
@@ -31,9 +38,12 @@ def run_experiment(rounds: int, file: str, seed: int = 0, **env_args):
 
     with open(file, "rb") as infile:
         data = pickle.load(infile)
+<<<<<<< HEAD
     
 
 
+=======
+>>>>>>> c3b7eeb... Ran first few experiments
     env = Environment(**env_args)
     env.random_assignment()
     a_segregation, b_segregation = env.simulate(rounds)
@@ -60,6 +70,7 @@ exp1 = {
     "gammas_a": a_class["x"].values,
     "gammas_b": b_class["x"].values,
 }
+<<<<<<< HEAD
 
 exp2 = deepcopy(exp1)
 exp2.update({
@@ -109,9 +120,50 @@ step_down_exps = [
     }
     for alpha in np.linspace(0, 0.9, 10)
 ]
-
-
 # %%
 
 for env_args, seed in tqdm(itertools.product(step_down_exps, range(10))):
     run_experiment(100, "segregation_experiments2.pkl", seed, **env_args)
+=======
+
+exp2 = deepcopy(exp1)
+exp2.update({
+    "alphas": 0.5,
+    "betas": 0.5
+})
+
+gamma_std_a = np.var(a_class["x"].values) ** 0.5
+gamma_std_b = np.var(b_class["x"].values) ** 0.5
+
+pop_prop_a = len(a_class) / (len(a_class) + len(b_class))
+pop_prop_b = len(b_class) / (len(a_class) + len(b_class))
+
+#gammas and betas equal to proportion of population. Fair preferences.
+
+gamma_a_range = np.linspace(0.05, pop_prop_a, 10)
+gamma_b_range = np.linspace(0.05, pop_prop_b, 10)
+
+exps = [
+    {
+        "num_firms": 100,
+        "capacities": 150,
+        "alphas": alpha,
+        "betas": beta,
+        "num_group_a": len(a_class),
+        "num_group_b": len(b_class),
+        "gammas_a": np.random.normal(gamma_a, gamma_std_a, len(a_class)),
+        "gammas_b": np.random.normal(gamma_b, gamma_std_b, len(b_class)),
+    }
+    for (alpha, beta), gamma_a, gamma_b
+    in itertools.product(zip([0.5, 1], [0.5, 1]), gamma_a_range, gamma_b_range)
+]
+
+exps.insert(0, exp1)
+exps.insert(1, exp2)
+
+
+# %%
+
+for env_args, seed in tqdm(itertools.product(exps, range(10))):
+    run_experiment(200, "segregation_experiments.pkl", seed, **env_args)
+>>>>>>> c3b7eeb... Ran first few experiments
