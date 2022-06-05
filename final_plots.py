2c09b804819a791b9d2de56618b1ff7764bceef3
clinton0313 <clinton0313@gmail.com>
2022-06-05 12:08:46 +0200

    Some final plotting.

---
+++ final_plots.py
@@ -0,0 +1,148 @@
+#%%
+
+import matplotlib
+import matplotlib.style
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
+
+from plot_segregation import parse_results
+from typing import Optional
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+matplotlib.use("tkagg")
+matplotlib.style.use("seaborn-bright")
+matplotlib.rcParams.update({
+    "figure.figsize": (12, 12),
+    "axes.spines.top": False,
+    "axes.spines.right": False
+})
+
+
+employer = pd.read_pickle("employer_experiments.pkl")
+employee = pd.read_pickle("employee_experiments.pkl")
+
+
+employer = parse_results(employer)
+employee = parse_results(employee)
+
+
+#%%
+
+def plot_employer(res: pd.DataFrame, group: str = "alphas", savepath: Optional[str] = None) -> plt.Figure:
+    
+    data = res[res[group]== 1]
+    other_group = "alphas" if group == "betas" else "betas"
+    other_group_title = other_group.replace("s", "").capitalize()
+    group_letter = list(group)[0]
+    other_group_letter = list(group)[0]
+
+    fig, ax = plt.subplots()
+    
+    for value in np.linspace(0.1, 0.5, 5):
+        record = data[data[other_group] == value]
+        x = range(record.rounds.values[0] + 1)
+        ax.plot(
+            x, 
+            record[f"{group_letter}_isolation"].values[0], 
+            color="tab:blue",
+            label=f"Isolation with {other_group_title} = {round(value, 1)}",
+            alpha=2*value
+        )
+    for value in np.linspace(0.1, 0.5, 5):
+        record = data[data[other_group] == value]
+        x = range(record.rounds.values[0] + 1)
+        ax.plot(
+            x, 
+            record[f"{group_letter}_dissimilarity"].values[0], 
+            color="tab:red",
+            label=f"Dissimilarity with {other_group_title} = {round(value, 1)}",
+            alpha=2*value
+        )
+
+    ax.set_ylim(0.2, 1.01)
+    ax.set_xlim(0, 35)
+    
+    ax.legend()
+    ax.set_xlabel("Rounds")
+    ax.set_ylabel("Segregation")
+    ax.set_title(f"Segregation of Group {list(group.capitalize())[0]}")
+
+    if savepath is not None:
+        fig.savefig(
+            os.path.join(savepath, f"changing_{other_group}_segregation.png"),
+            facecolor="white",
+            transparent=False
+        )
+        plt.close(fig)
+        fig.clear()
+    else:
+        return fig
+
+# %%
+
+plot_employer(employer, "alphas", "final_figs")
+plot_employer(employer, "betas", "final_figs")
+# %%
+
+def find_closest(value: float, a: np.ndarray) -> float:
+    distance = np.abs(a - value)
+    return a[np.argmin(distance)]
+
+def plot_final_segregation(
+    res: pd.DataFrame, 
+    group: str = "alphas", 
+    savepath: Optional[str] = None
+) -> plt.Figure:
+
+    other_group = "alphas" if group == "betas" else "betas"
+    other_group_title = other_group.replace("s", "").capitalize()
+    fig, axes = plt.subplots(1, 2, figsize=(24, 12), tight_layout=True)
+
+    group_letter = list(group)[0]
+    other_group_letter = list(other_group)[0]
+    
+    res[f"Gamma {other_group_letter.capitalize()} Mean"] = res[f"gamma_{other_group_letter}_mean"]
+
+    sns.scatterplot(
+        res[f"gamma_{group_letter}_mean"], 
+        res[f"{group_letter}_final_isolation"],
+        hue=res[f"Gamma {other_group_letter.capitalize()} Mean"],
+        ax=axes[0],
+        label=f"Isolation of Group {group_letter.capitalize()}"
+    )
+
+    sns.scatterplot(
+        res[f"gamma_{group_letter}_mean"], 
+        res[f"{group_letter}_final_dissimilarity"],
+        hue=res[f"Gamma {other_group_letter.capitalize()} Mean"],
+        ax=axes[1],
+        label=f"Dissimilarity of Group {group_letter.capitalize()}"
+    )
+
+    for ax in axes:
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+    
+    fig.suptitle(f"Final Segregation of Group {list(group.capitalize())[0]}", fontsize=14)
+    fig.supxlabel(f"Group {group_letter.capitalize()} Gamma")
+    fig.supylabel("Final Segregation")
+
+
+    if savepath is not None:
+        fig.savefig(
+            os.path.join(savepath, f"{group_letter}_final_segregation.png"),
+            facecolor="white",
+            transparent=False
+        )
+        plt.close(fig)
+        fig.clear()
+    else:
+        return fig
+
+plot_final_segregation(employee, "alphas", "final_figs")
+plot_final_segregation(employee, "betas", "final_figs")
+# %%
--- segregation_experiments.py
+++ segregation_experiments.py
@@ -1,15 +1,6 @@
 #%%
 import itertools
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import random
-=======
->>>>>>> c3b7eeb... Ran first few experiments
-=======
->>>>>>> c3b7eeb... Ran first few experiments
-=======
->>>>>>> 202b5910818732903ce4c3e77dd71cbf468b878c
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -26,18 +17,9 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 def run_experiment(rounds: int, file: str, seed: int = 0, **env_args):
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     np.random.seed(seed)
     random.seed(seed)
 
-=======
->>>>>>> c3b7eeb... Ran first few experiments
-=======
->>>>>>> c3b7eeb... Ran first few experiments
-=======
->>>>>>> 202b5910818732903ce4c3e77dd71cbf468b878c
     if not os.path.isfile(file):
         data = pd.DataFrame(
             columns = (
@@ -50,18 +32,9 @@ def run_experiment(rounds: int, file: str, seed: int = 0, **env_args):
 
     with open(file, "rb") as infile:
         data = pickle.load(infile)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     
 
 
-=======
->>>>>>> c3b7eeb... Ran first few experiments
-=======
->>>>>>> c3b7eeb... Ran first few experiments
-=======
->>>>>>> 202b5910818732903ce4c3e77dd71cbf468b878c
     env = Environment(**env_args)
     env.random_assignment()
     a_segregation, b_segregation = env.simulate(rounds)
@@ -88,9 +61,6 @@ exp1 = {
     "gammas_a": a_class["x"].values,
     "gammas_b": b_class["x"].values,
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 exp2 = deepcopy(exp1)
 exp2.update({
@@ -106,8 +76,8 @@ pop_prop_b = len(b_class) / (len(a_class) + len(b_class))
 
 #gammas and betas equal to proportion of population. Fair preferences.
 
-gamma_a_range = np.linspace(pop_prop_a, a_class["x"].mean(), 10)
-gamma_b_range = np.linspace(pop_prop_b, b_class["x"].mean(), 10)
+gamma_a_range = np.linspace(0, 1, 11)
+gamma_b_range = np.linspace(0, 1, 11)
 
 exps = [
     {
@@ -132,70 +102,20 @@ step_down_exps = [
         "num_firms": 100,
         "capacities": 150,
         "alphas": alpha,
-        "betas": 1,
+        "betas": 1.0,
         "num_group_a": len(a_class),
         "num_group_b": len(b_class),
         "gammas_a": a_class["x"].values,
         "gammas_b": b_class["x"].values,
     }
     for alpha in np.linspace(0, 0.9, 10)
+    # for beta in np.linspace(0, 0.9, 10)
 ]
 # %%
 
-for env_args, seed in tqdm(itertools.product(step_down_exps, range(10))):
-    run_experiment(100, "segregation_experiments2.pkl", seed, **env_args)
-=======
-=======
->>>>>>> c3b7eeb... Ran first few experiments
-=======
->>>>>>> 202b5910818732903ce4c3e77dd71cbf468b878c
+for env_args, seed in tqdm(itertools.product(step_down_exps, range(5))):
+    run_experiment(100, "employer_experiments.pkl", seed, **env_args)
 
-exp2 = deepcopy(exp1)
-exp2.update({
-    "alphas": 0.5,
-    "betas": 0.5
-})
+# for env_args, seed in tqdm(itertools.product(exps, range(5))):
+#     run_experiment(100, "employee_experiments.pkl", seed, **env_args)
 
-gamma_std_a = np.var(a_class["x"].values) ** 0.5
-gamma_std_b = np.var(b_class["x"].values) ** 0.5
-
-pop_prop_a = len(a_class) / (len(a_class) + len(b_class))
-pop_prop_b = len(b_class) / (len(a_class) + len(b_class))
-
-#gammas and betas equal to proportion of population. Fair preferences.
-
-gamma_a_range = np.linspace(0.05, pop_prop_a, 10)
-gamma_b_range = np.linspace(0.05, pop_prop_b, 10)
-
-exps = [
-    {
-        "num_firms": 100,
-        "capacities": 150,
-        "alphas": alpha,
-        "betas": beta,
-        "num_group_a": len(a_class),
-        "num_group_b": len(b_class),
-        "gammas_a": np.random.normal(gamma_a, gamma_std_a, len(a_class)),
-        "gammas_b": np.random.normal(gamma_b, gamma_std_b, len(b_class)),
-    }
-    for (alpha, beta), gamma_a, gamma_b
-    in itertools.product(zip([0.5, 1], [0.5, 1]), gamma_a_range, gamma_b_range)
-]
-
-exps.insert(0, exp1)
-exps.insert(1, exp2)
-
-
-# %%
-
-for env_args, seed in tqdm(itertools.product(exps, range(10))):
-<<<<<<< HEAD
-<<<<<<< HEAD
-    run_experiment(200, "segregation_experiments.pkl", seed, **env_args)
->>>>>>> c3b7eeb... Ran first few experiments
-=======
-    run_experiment(200, "segregation_experiments.pkl", seed, **env_args)
->>>>>>> c3b7eeb... Ran first few experiments
-=======
-    run_experiment(200, "segregation_experiments.pkl", seed, **env_args)
->>>>>>> 202b5910818732903ce4c3e77dd71cbf468b878c
